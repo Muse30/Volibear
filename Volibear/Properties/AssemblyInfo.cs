23911f3893cf854e9bb9b9a9075e14d5a670c519
Muse30 <jrcrack009@gmail.com>
2016-04-18 15:46:40 -0600

    v
    v

--- Volibear/Properties/AssemblyInfo.cs
+++ Volibear/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Volibea")]
+[assembly: AssemblyTitle("Shaco")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Volibea")]
+[assembly: AssemblyProduct("Shaco")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("00d3d0ea-a5ab-4492-9e96-fc9b01208e63")]
+[assembly: Guid("b0923062-d4c8-4b61-b40f-91a9afc32a41")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
--- Volibear/Volibear.cs
+++ Volibear/Volibear.cs
@@ -1,74 +1,29 @@
-﻿namespace Volibear
+﻿using System;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Events;
+
+namespace Volibear
 {
-    using System;
-    using System.Linq;
-
-    using EloBuddy;
-    using EloBuddy.SDK;
-    using EloBuddy.SDK.Enumerations;
-    using EloBuddy.SDK.Rendering;
-    using EloBuddy.SDK.Events;
-    using EloBuddy.SDK.Menu;
-    using EloBuddy.SDK.Menu.Values;
-    using Color = System.Drawing.Color;
-
-    using SharpDX;
-    using System.Drawing;
-
     internal class Volibear
     {
-        public static Spell.Active Q { get; private set; }
 
-        public static Spell.Targeted W { get; private set; }
-
-        public static Spell.Active E { get; private set; }
-
-        public static Spell.Active R { get; private set; }
-
-        public static Spell.Targeted Smitee;
-
-        public static readonly string[] SmiteableUnits =
+        public static Spell.Targeted W;
+        public static Spell.Active Q, E, R;
+        public static Menu VoliMenu, ComboMenu, HarassMenu, LaneMenu, MiscMenu, JungleMenu, KSMenu, DrawMenu, SmiteMenu;
+      
+        public static AIHeroClient Player
         {
-            "SRed", "SBlue", "SDragon", "SBaron"
-        };
-
-        private static readonly int[] SmiteRed = { 3715, 1415, 1414, 1413, 1412 };
-        private static readonly int[] SmiteBlue = { 3706, 1403, 1402, 1401, 1400 };
-
-        public static double TotalDamage = 0;
-
-        public static AIHeroClient _player { get; set; }
-
-        public static Menu ComboMenu { get; private set; }
-
-        public static Menu HarassMenu { get; private set; }
-
-        public static Menu LaneMenu { get; private set; }
-
-        public static Menu MiscMenu { get; private set; }
-
-        public static Menu KSMenu { get; private set; }
-
-        public static Menu JungleMenu { get; private set; }
-
-        public static SpellSlot Smite;
-
-        public static Menu DrawMenu { get; private set; }
-
-        private static Menu VoliMenu;
-
-        private static void Main(string[] args)
-        {
-            Loading.OnLoadingComplete += OnLoad;
+            get { return ObjectManager.Player; }
         }
 
-        private static void OnLoad(EventArgs args)
+        public static void Loading_OnLoadingComplete(EventArgs args)
         {
-            if (ObjectManager.Player.BaseSkinName != "Volibear")
-            {
-                return;
-            }
-
+            if (Player.Hero != Champion.Volibear) return;
+            Chat.Print("Volibear loaded");
             VoliMenu = MainMenu.AddMenu("Volibear", "Volibear");
             VoliMenu.AddGroupLabel("Crazy Voli!");
             ComboMenu = VoliMenu.AddSubMenu("Combo");
@@ -80,13 +35,12 @@
             ComboMenu.Add("UseItems", new CheckBox("Use Items"));
             ComboMenu.Add("Wcount", new Slider("Enemy health % to use W", 100, 0, 100));
             ComboMenu.Add("Rcount", new Slider("Num of Enemy in Range to Ult", 2, 1, 5));
-            ComboMenu.Add("BlueSmite", new CheckBox("KS with Smite"));
-            ComboMenu.Add("Redsmite", new CheckBox("Combo with Smite (red)"));
+
 
             HarassMenu = VoliMenu.AddSubMenu("Harass");
             HarassMenu.AddGroupLabel("Harass Settings");
             HarassMenu.Add("Ehrs", new CheckBox("Use E in Harass"));
-        
+
 
             LaneMenu = VoliMenu.AddSubMenu("Farm");
             LaneMenu.AddGroupLabel("LaneClear Settings");
@@ -102,17 +56,12 @@
             JungleMenu.Add("JungleW", new CheckBox("Use W"));
             JungleMenu.Add("JungleE", new CheckBox("Use E"));
             JungleMenu.Add("JCM", new Slider("Mana %", 30, 0, 100));
-            JungleMenu.Add("Sbaron", new CheckBox("Smite Baron"));
-            JungleMenu.Add("sDragon", new CheckBox("Smite Dragon"));
-            JungleMenu.Add("SBlue", new CheckBox("Smite Blue"));
-            JungleMenu.Add("SRed", new CheckBox("Smite Red"));
+
 
 
             MiscMenu = VoliMenu.AddSubMenu("Misc");
             MiscMenu.AddGroupLabel("Misc Settings");
             MiscMenu.Add("gapcloserW", new CheckBox("Anti-GapCloser W"));
-            MiscMenu.Add("smiteActive",
-              new KeyBind("Smite Active (toggle)", true, KeyBind.BindTypes.PressToggle, 'H'));
 
             KSMenu = VoliMenu.AddSubMenu("ks");
             KSMenu.AddGroupLabel("killsteal Settings");
@@ -122,67 +71,66 @@
             DrawMenu = VoliMenu.AddSubMenu("Drawings");
             DrawMenu.AddGroupLabel("Drawing Settings");
             DrawMenu.Add("DrawWE", new CheckBox("Draw W and E"));
-            DrawMenu.Add("smitestatus", new CheckBox("Draw Smite Status"));
+            DrawMenu.Add("smitestatus1", new CheckBox("Draw Smite Status"));
 
 
 
             Q = new Spell.Active(SpellSlot.Q, 750);
             W = new Spell.Targeted(SpellSlot.W, 395);
             E = new Spell.Active(SpellSlot.E, 415);
-            R = new Spell.Active(SpellSlot.R, (uint)Player.Instance.GetAutoAttackRange());
+            R = new Spell.Active(SpellSlot.R, (uint)Player.GetAutoAttackRange());
 
             Game.OnUpdate += OnUpdate;
             Drawing.OnDraw += Drawing_OnDraw;
             Orbwalker.OnPreAttack += Orbwalker_OnPreAttack;
             Gapcloser.OnGapcloser += Gapcloser_OnGapcloser;
-            Game.OnUpdate += SmiteEvent;
-
         }
 
-        private static void OnUpdate(EventArgs args)
-        {
-            _player = ObjectManager.Player;
 
-          
-          {
+        private static void OnUpdate(EventArgs args)
+        
+
+            {
                 if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo)) Combo();
                 if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass)) Harass();
                 if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear)) LaneClear();
-                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear)) JungleClear(); 
-                
-                }
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear)) JungleClear();
 
-                KillSteal();
-            }
+
+            KillSteal();
+            Smitez.Smitemethod();
+        }
+
+
 
 
         static void Orbwalker_OnPreAttack(AttackableUnit target, Orbwalker.PreAttackArgs args)
         {
-        if (ComboMenu["UseR"].Cast<CheckBox>().CurrentValue && R.IsReady() && args.Target.IsEnemy &&
-            args.Target.IsValid &&
-            _player.CountEnemiesInRange(300) >= ComboMenu["Rcount"].Cast<Slider>().CurrentValue)
+            if (ComboMenu["UseR"].Cast<CheckBox>().CurrentValue && R.IsReady() && args.Target.IsEnemy &&
+                args.Target.IsValid &&
+                Player.CountEnemiesInRange(300) >= ComboMenu["Rcount"].Cast<Slider>().CurrentValue)
 
-        {
-            R.Cast();
+            {
+                R.Cast();
+            }
         }
-    }
 
 
         private static void Drawing_OnDraw(EventArgs args)
         {
             if (DrawMenu["DrawWE"].Cast<CheckBox>().CurrentValue)
             {
-                Drawing.DrawCircle(_player.Position, E.Range, Color.CadetBlue);
+                Drawing.DrawCircle(Player.Position, E.Range, System.Drawing.Color.CadetBlue);
             }
             var heropos = Drawing.WorldToScreen(ObjectManager.Player.Position);
-            var green = Color.LimeGreen;
-            var red = Color.IndianRed;
-            if (DrawMenu["smitestatus"].Cast<CheckBox>().CurrentValue)
+            var green = System.Drawing.Color.LimeGreen;
+            var red = System.Drawing.Color.IndianRed;
+            if (DrawMenu["smitestatus1"].Cast<CheckBox>().CurrentValue)
             {
                 Drawing.DrawText(heropos.X - 40, heropos.Y + 20, System.Drawing.Color.FloralWhite, "Smite:");
                 Drawing.DrawText(heropos.X + 10, heropos.Y + 20,
-                    MiscMenu["smiteActive"].Cast<KeyBind>().CurrentValue ? System.Drawing.Color.LimeGreen : System.Drawing.Color.Red,
-                    MiscMenu["smiteActive"].Cast<KeyBind>().CurrentValue ? "On" : "Off");
+                    SmiteMenu["smiteActive"].Cast<KeyBind>().CurrentValue ? System.Drawing.Color.LimeGreen : System.Drawing.Color.Red,
+                    SmiteMenu["smiteActive"].Cast<KeyBind>().CurrentValue ? "On" : "Off");
 
             }
         }
@@ -210,7 +158,7 @@
         public static double WDamage(Obj_AI_Base target)
         {
             return (new double[] { 80, 125, 170, 215, 260 }[W.Level - 1] +
-                    ((_player.MaxHealth - (498.48f + (86f * (_player.Level - 1f)))) * 0.15f)) *
+                    ((Player.MaxHealth - (498.48f + (86f * (Player.Level - 1f)))) * 0.15f)) *
                    ((target.MaxHealth - target.Health) / target.MaxHealth + 1);
         }
 
@@ -220,11 +168,11 @@
             if (target == null || target.IsZombie) return;
 
 
-            if (_player.Distance(target) <= Q.Range && Q.IsReady() && (ComboMenu["UseQ"].Cast<CheckBox>().CurrentValue))
+            if (Player.Distance(target) <= Q.Range && Q.IsReady() && (ComboMenu["UseQ"].Cast<CheckBox>().CurrentValue))
             {
-               Q.Cast();
+                Q.Cast();
             }
-            if (_player.Distance(target) <= E.Range && E.IsReady() && (ComboMenu["UseE"].Cast<CheckBox>().CurrentValue))
+            if (Player.Distance(target) <= E.Range && E.IsReady() && (ComboMenu["UseE"].Cast<CheckBox>().CurrentValue))
             {
                 E.Cast();
             }
@@ -250,7 +198,7 @@
         private static void Gapcloser_OnGapcloser(AIHeroClient sender, Gapcloser.GapcloserEventArgs e)
         {
             var T = TargetSelector.GetTarget(W.Range, DamageType.Physical);
-            if (_player.IsDead || !sender.IsEnemy || !sender.IsValidTarget(W.Range) || !W.IsReady() || !MiscMenu["gapcloserW"].Cast<CheckBox>().CurrentValue) return;
+            if (Player.IsDead || !sender.IsEnemy || !sender.IsValidTarget(W.Range) || !W.IsReady() || !MiscMenu["gapcloserW"].Cast<CheckBox>().CurrentValue) return;
 
             W.Cast(T);
         }
@@ -259,30 +207,30 @@
 
         internal static void UseItems(Obj_AI_Base target)
         {
-            var KhazixServerPosition = _player.ServerPosition.To2D();
+            var KhazixServerPosition = Player.ServerPosition.To2D();
             var targetServerPosition = target.ServerPosition.To2D();
 
-            if (Item.CanUseItem(ItemId.Ravenous_Hydra_Melee_Only) && 400 > _player.Distance(target))
+            if (Item.CanUseItem(ItemId.Ravenous_Hydra_Melee_Only) && 400 > Player.Distance(target))
             {
                 Item.UseItem(ItemId.Ravenous_Hydra_Melee_Only);
             }
-            if (Item.CanUseItem(ItemId.Tiamat_Melee_Only) && 400 > _player.Distance(target))
+            if (Item.CanUseItem(ItemId.Tiamat_Melee_Only) && 400 > Player.Distance(target))
             {
                 Item.UseItem(ItemId.Tiamat_Melee_Only);
             }
-            if (Item.CanUseItem(ItemId.Titanic_Hydra) && 400 > _player.Distance(target))
+            if (Item.CanUseItem(ItemId.Titanic_Hydra) && 400 > Player.Distance(target))
             {
                 Item.UseItem(ItemId.Titanic_Hydra);
             }
-            if (Item.CanUseItem(ItemId.Blade_of_the_Ruined_King) && 550 > _player.Distance(target))
+            if (Item.CanUseItem(ItemId.Blade_of_the_Ruined_King) && 550 > Player.Distance(target))
             {
                 Item.UseItem(ItemId.Blade_of_the_Ruined_King);
             }
-            if (Item.CanUseItem(ItemId.Youmuus_Ghostblade) && _player.GetAutoAttackRange() > _player.Distance(target))
+            if (Item.CanUseItem(ItemId.Youmuus_Ghostblade) && Player.GetAutoAttackRange() > Player.Distance(target))
             {
                 Item.UseItem(ItemId.Youmuus_Ghostblade);
             }
-            if (Item.CanUseItem(ItemId.Bilgewater_Cutlass) && 550 > _player.Distance(target))
+            if (Item.CanUseItem(ItemId.Bilgewater_Cutlass) && 550 > Player.Distance(target))
             {
                 Item.UseItem(ItemId.Bilgewater_Cutlass);
             }
@@ -297,7 +245,7 @@
                 return;
             }
 
-            if (HarassMenu["Ehrs"].Cast<CheckBox>().CurrentValue && _player.Distance(target) <= E.Range && E.IsReady())
+            if (HarassMenu["Ehrs"].Cast<CheckBox>().CurrentValue && Player.Distance(target) <= E.Range && E.IsReady())
             {
                 E.Cast();
             }
@@ -305,9 +253,9 @@
 
         private static void JungleClear()
         {
-            var Mob = EntityManager.MinionsAndMonsters.GetJungleMonsters(_player.ServerPosition, E.Range).OrderBy(x => x.MaxHealth).ToList();
+            var Mob = EntityManager.MinionsAndMonsters.GetJungleMonsters(Player.ServerPosition, E.Range).OrderBy(x => x.MaxHealth).ToList();
 
-            if (_player.ManaPercent > JungleMenu["JCM"].Cast<Slider>().CurrentValue)
+            if (Player.ManaPercent > JungleMenu["JCM"].Cast<Slider>().CurrentValue)
             {
                 if (JungleMenu["JungleQ"].Cast<CheckBox>().CurrentValue && Q.IsReady())
                 {
@@ -343,89 +291,16 @@
             }
         }
 
-        public static void SetSmiteSlot()
+        public static bool HasSpell(string s)
         {
-            SpellSlot smiteSlot;
-            if (SmiteBlue.Any(x => _player.InventoryItems.FirstOrDefault(a => a.Id == (ItemId)x) != null))
-                smiteSlot = _player.GetSpellSlotFromName("s5_summonersmiteplayerganker");
-            else if (
-                SmiteRed.Any(
-                    x => _player.InventoryItems.FirstOrDefault(a => a.Id == (ItemId)x) != null))
-                smiteSlot = _player.GetSpellSlotFromName("s5_summonersmiteduel");
-            else
-                smiteSlot = _player.GetSpellSlotFromName("summonersmite");
-            Smitee = new Spell.Targeted(smiteSlot, 500);
+            return EloBuddy.Player.Spells.FirstOrDefault(o => o.SData.Name.Contains(s)) != null;
         }
-
-
-        public static int GetSmiteDamage()
-        {
-            var level = _player.Level;
-            int[] smitedamage =
-            {
-                20*level + 370,
-                30*level + 330,
-                40*level + 240,
-                50*level + 100
-            };
-            return smitedamage.Max();
-        }
-
-        private static void SmiteEvent(EventArgs args)
-        {
-            SetSmiteSlot();
-            if (!Smitee.IsReady() || _player.IsDead) return;
-            if (MiscMenu["smiteActive"].Cast<KeyBind>().CurrentValue)
-            {
-                var unit =
-                    EntityManager.MinionsAndMonsters.Monsters
-                        .Where(
-                            a =>
-                                SmiteableUnits.Contains(a.BaseSkinName) && a.Health < GetSmiteDamage() &&
-                                JungleMenu[a.BaseSkinName].Cast<CheckBox>().CurrentValue)
-                        .OrderByDescending(a => a.MaxHealth)
-                        .FirstOrDefault();
-
-                if (unit != null)
-                {
-                    Smitee.Cast(unit);
-                    return;
-                }
-            }
-            if (ComboMenu["BlueSmite"].Cast<CheckBox>().CurrentValue &&
-                Smitee.Handle.Name == "s5_summonersmiteplayerganker")
-            {
-                foreach (
-                    var target in
-                        EntityManager.Heroes.Enemies
-                            .Where(h => h.IsValidTarget(Smitee.Range) && h.Health <= 20 + 8 * _player.Level))
-                {
-                    Smitee.Cast(target);
-                    return;
-                }
-            }
-            if (ComboMenu["Redsmite"].Cast<CheckBox>().CurrentValue &&
-                Smitee.Handle.Name == "s5_summonersmiteduel" &&
-                Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
-            {
-                foreach (
-                    var target in
-                        EntityManager.Heroes.Enemies
-                            .Where(h => h.IsValidTarget(Smitee.Range)).OrderByDescending(TargetSelector.GetPriority)
-                    )
-                {
-                    Smitee.Cast(target);
-                    return;
-                }
-            }
-        }
-
 
         private static void LaneClear()
         {
-            var allMinions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, _player.ServerPosition, Q.Range);
+            var allMinions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.ServerPosition, Q.Range);
 
-            if (_player.ManaPercent > LaneMenu["LCM"].Cast<Slider>().CurrentValue)
+            if (Player.ManaPercent > LaneMenu["LCM"].Cast<Slider>().CurrentValue)
             {
                 if (LaneMenu["LaneW"].Cast<CheckBox>().CurrentValue && Q.IsReady())
                 {
